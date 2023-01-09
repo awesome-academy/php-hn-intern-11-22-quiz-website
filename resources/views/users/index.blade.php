7ac10ddf5a477f57e70adf47d035e08eec4ce8a5
Pham Vu Huy <pham.vu.huy@sun-asterisk.com>
2023-01-09 09:35:11 +0700

    Create User

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -13,10 +13,10 @@
                 <div class="card-header border-0">
                     <div class="row align-items-center">
                         <div class="col-8">
-                            <h3 class="mb-0">Users</h3>
+                            <h3 class="mb-0">{{ __('Users') }}</h3>
                         </div>
                         <div class="col-4 text-right">
-                            <a href="" class="btn btn-sm btn-primary">Add user</a>
+                            <a href="{{ route('users.create') }}" class="btn btn-sm btn-primary">{{ __('Add user') }}</a>
                         </div>
                     </div>
                 </div>
@@ -24,11 +24,11 @@
                     <table class="table align-items-center table-flush">
                         <thead class="thead-light">
                             <tr>
-                                <th scope="col">No</th>
-                                <th scope="col">Username</th>
-                                <th scope="col">Email</th>
-                                <th scope="col">First Name</th>
-                                <th scope="col">Last Name</th>
+                                <th scope="col">{{ __('No') }}</th>
+                                <th scope="col">{{ __('Username') }}</th>
+                                <th scope="col">{{ __('Email') }}</th>
+                                <th scope="col">{{ __('First Name') }}</th>
+                                <th scope="col">{{ __('Last Name') }}</th>
                                 <th scope="col"></th>
                             </tr>
                         </thead>
@@ -56,8 +56,8 @@
                                             <i class="fas fa-ellipsis-v"></i>
                                         </a>
                                         <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
-                                            <a class="dropdown-item" href="{{route('users.edit', $user->id)}}">Edit</a>
-                                            <a class="dropdown-item" href="{{route('users.destroy', $user->id)}}">Delete</a>
+                                            <a class="dropdown-item" href="{{route('users.edit', $user->id)}}">{{ __('Edit') }}</a>
+                                            <a class="dropdown-item" href="{{route('users.destroy', $user->id)}}">{{ __('Delete') }}</a>
                                         </div>
                                     </div>
                                 </td>
@@ -65,11 +65,6 @@
                             @endforeach
                         </tbody>
                     </table>
-                </div>
-                <div class="card-footer py-4">
-                    <nav class="d-flex justify-content-end" aria-label="...">
-
-                    </nav>
                 </div>
             </div>
         </div>
