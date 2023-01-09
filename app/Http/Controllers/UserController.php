0ab7d7ae23c4164e8ecd3c4001ad69cd577c3594
Pham Vu Huy <pham.vu.huy@sun-asterisk.com>
2023-01-09 10:47:27 +0700

    Delete user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -96,6 +96,9 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $user = User::findOrFail($id);
+        $user->delete();
+
+        return redirect()->route('users.index');
     }
 }
