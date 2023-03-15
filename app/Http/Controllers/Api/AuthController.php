fdbe819e056c6f6a23cd1c6ab2417f5a3706344a
Pham Vu Huy <pham.vu.huy@sun-asterisk.com>
2023-03-15 16:48:54 +0700

    Register API

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,11 +4,22 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\LoginRequest;
+use App\Http\Requests\Api\RegisterRequest;
+use App\Repositories\User\UserRepositoryInterface;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+    protected $userRepo;
+
+    public function __construct(UserRepositoryInterface $userRepo)
+    {
+        $this->userRepo = $userRepo;
+    }
+
     public function login(LoginRequest $request)
     {
         try {
@@ -30,6 +41,30 @@ class AuthController extends Controller
                 'code' => 200,
                 'access_token' => $token,
                 'token_type' => 'Bearer',
+            ]);
+        } catch (\Exception $ex) {
+            return response()->json([
+                'code' => 500,
+                'error' => $ex->getMessage(),
+            ]);
+        }
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        try {
+            $this->userRepo->create([
+                'username' => $request->username,
+                'email' => $request->email,
+                'first_name' => $request->first_name,
+                'last_name' => $request->last_name,
+                'password' => Hash::make($request->password),
+                'role_id' => User::ROLE_USER,
+            ]);
+
+            return response()->json([
+                'code' => 200,
+                'message' => __('Register Successfully'),
             ]);
         } catch (\Exception $ex) {
             return response()->json([
